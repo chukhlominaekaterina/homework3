235813911de43d6d6f469e6bc3d557d934a410af
chukhlominaekaterina <sectumsempragpp@gmail.com>
2024-11-18 19:11:17 +1000

    Revert "Dz4"

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,87 @@
 import 'package:flutter/material.dart';
-import 'package:homework3/func/appin.dart';
+
 void main() {
   runApp(const MyApp());
 }
 
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
 
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
+      ),
+      home: const MyHomePage(title: 'Chukhlomina Ekaterina'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key, required this.title});
+
+
+  final String title;
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  int _counter = 0;
+
+  void _incrementCounter() {
+    setState(() {
+
+      _counter++;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+        title: Text(widget.title),
+      ),
+      body: Center(
+        child: Column(
+
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Icon(
+              Icons.account_circle,
+              color: Colors.blueAccent,
+              size: 300.0,
+              semanticLabel: 'Text to announce in accessibility modes',
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                const Text('24.08.2001'),
+                const Text('      +79149746967'),
+                TextButton(onPressed: _incrementCounter, child: const Text('subscribe')),
+              ],
+            ),
+            const Text('ask me a question:'),
+            const Text('eofoauhvonsjagnondlfmc'),
+            TextField(),
+            Text('followers: '
+              '$_counter',
+              style: Theme.of(context).textTheme.headlineMedium,
+            ),
+          ],
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: _incrementCounter,
+        tooltip: 'Increment',
+        child: const Icon(Icons.add),
+      ), // This trailing comma makes auto-formatting nicer for build methods.
+    );
+  }
+}
